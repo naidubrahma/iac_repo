f37029b185641831dbadc02d56f1b430c150fb64
gladwin90 <68490658+gladwin90@users.noreply.github.com>
2024-01-08 11:15:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_dynamodb_table" "example_1" {
 */
 terraform {
   backend "s3" {
-    bucket         = "san-bucket63838146"
+    bucket         = "san-bucket-john"
     dynamodb_table = "san-table-john"
     key            = "global/mystatefile/terraform.tfstate"
     region         = "us-west-2"
